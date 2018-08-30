0674c178540779d7101df9fdcfc264362c9eeee5
semiprojecthifive <rlruddnr828@gmail.com>
2018-08-30 18:50:57 +0900

    alpha(beta 1차 통합)

--- hifiveWebProject/src/user/controller/EmailSendServlet.java
+++ hifiveWebProject/src/user/controller/EmailSendServlet.java
@@ -33,7 +33,7 @@ public class EmailSendServlet extends HttpServlet {
 	        super();
 	        // TODO Auto-generated constructor stub
 	    }
-
+ 
 		/**
 		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 		 */
